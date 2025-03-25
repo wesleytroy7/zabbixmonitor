560f02b13c81d7ffc6d5ce71dee1fe86fa33d27b
wesleytroy7 <wesleyaf@proton.me>
2025-03-25 01:11:27 -0300

    adionado compose e script

---
+++ diretorio.sh
@@ -0,0 +1,7 @@
+mkdir -p ./zabbix/usr/lib/zabbix/alertscripts  \
+./zabbix/usr/lib/zabbix/externalscripts \
+./zabbix/var/lib/zabbix/modules \
+./zabbix/var/lib/zabbix/enc \
+./zabbix/var/lib/zabbix/ssh_keys  \
+./zabbix/var/lib/zabbix/ssl/certs  \
+./pgdata/var/lib/postgresql/data
